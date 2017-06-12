33e533e79a7b42a3e29c05db954a05a410c638de
Hack Reactor Student <student@hackreactor.com>
2017-06-12 16:41:41 -0700

    wip

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,19 @@
 var models = require('../models');
+var url = require('url');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      let pathname = url.parse(req.url).pathname;
+      if (pathname === '/classes/messages') {
+        models.messages.get(res, 200);
+      } else {
+        models.messages.get(res, 404);
+      }
+    }, // a function which handles a get request for all messages
+    post: function (req, res) {
+      models.messages.post(req, res, 201);
+    } // a function which handles posting a message to the database
   },
 
   users: {
--- server/models/index.js
+++ server/models/index.js
@@ -2,8 +2,22 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (res, statusCode) {
+      res.writeHead(statusCode, {'Content-Type': 'application/json'});
+      res.end(); // TODO get data from db
+    }, // a function which produces all the messages
+    post: function (req, res, statusCode) {
+      let result = '';
+      req.on('data', data => {
+        res += data;    
+      });
+      
+      req.on('end', () => {
+        // TODO insert data to db
+        res.writeHead(statusCode, null);
+        res.end();
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
