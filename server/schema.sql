56ed356d06127b8faa83d0de5f25ce6c3c3bca0b
Hack Reactor Student <student@hackreactor.com>
2017-06-12 20:00:40 -0700

    wip

--- server/schema.sql
+++ server/schema.sql
@@ -3,9 +3,23 @@ CREATE DATABASE chat;
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int not null,
+  message char(255) not null,
+  primary key (id)
+  -- foreign key messages (user) users (id)
 );
 
+CREATE TABLE users (
+  id Int not null, 
+  name char(20) not null,
+  primary key (id)
+);
+
+CREATE TABLE rooms (
+  id int not null,
+  roomname char(20) not null,
+  primary key (id)
+);
 /* Create other tables and define schemas for them here! */
 
 
@@ -15,3 +29,4 @@ CREATE TABLE messages (
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- select message from messages inner join users where users.name = 'sally' on users.id = messages.users
